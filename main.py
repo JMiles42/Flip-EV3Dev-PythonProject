26bf10fa2fe16f0d653fb9255bb44efaba783852
Jordan Miles <J@jmiles42.com>
2019-12-03 11:25:01 +1100

    Starting Code

---
+++ main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+DRIVE_MODE_FORWARD = 1
+DRIVE_MODE_BACKWARD = 2
+DRIVE_MODE_LEFT = 3
+DRIVE_MODE_RIGHT = 4
+DRIVE_MODE_STOP = 5
+DRIVE_MODE_UPRIGHT = 6
+DRIVE_MODE_UPSIDEDOWN = 7
+FORWARD_SPEED = 400
+TURN_SPEED = 100
+
+class RobotData:
+    mode = DRIVE_MODE_STOP
+    running = False
+    startingRot = 0
+    def __init__(self, _mode, _running, _rotation):
+        self.mode = _mode
+        self.running = _running
+        self.startingRot = _rotation
+    def SetMode(self, _modeNum):
+        self.mode = _modeNum
+
+
+mA = Motor(Port.A)
+mB = Motor(Port.B)
+mD = Motor(Port.D)
+
+rData = RobotData(DRIVE_MODE_STOP, False, mD.angle())
+
+def Drive(speed):
+    mA.run(speed)
+    mB.run(speed)
+
+def Turn(turnSpeed):
+    mA.run(-turnSpeed)
+    mB.run(turnSpeed)
+    
+def Stop():
+    mA.run(0)
+    mB.run(0)
+
+def DriveButtons():
+    buttons = brick.buttons()
+    if Button.CENTER in buttons:
+        rData.running = not rData.running
+    if Button.UP in buttons:
+        rData.SetMode(DRIVE_MODE_FORWARD)
+    if Button.DOWN in buttons:
+        rData.SetMode(DRIVE_MODE_BACKWARD)
+    if Button.LEFT in buttons:
+        rData.SetMode(DRIVE_MODE_UPSIDEDOWN)
+    if Button.RIGHT in buttons:
+        rData.SetMode(DRIVE_MODE_UPRIGHT)
+
+def FlipUpright():
+    mD.run_target(400, rData.startingRot)
+def FlipUpsideDown():
+    mD.run_target(400, rData.startingRot + 179)
+
+def DriveLogic():
+    if rData.running:
+        if rData.mode == DRIVE_MODE_FORWARD:
+            Drive(FORWARD_SPEED)
+        elif rData.mode == DRIVE_MODE_BACKWARD:
+            Drive(-FORWARD_SPEED)
+        elif rData.mode == DRIVE_MODE_LEFT:
+            Turn(TURN_SPEED)
+        elif rData.mode == DRIVE_MODE_RIGHT:
+            Turn(-TURN_SPEED)
+        elif rData.mode == DRIVE_MODE_UPRIGHT:
+            FlipUpright()
+        elif rData.mode == DRIVE_MODE_UPSIDEDOWN:
+            FlipUpsideDown()
+    else:
+        Stop()
+
+def DrivingLoop():
+    while True:
+        DriveLogic()
+
+t = Thread(target = DrivingLoop)
+t.start()
+
+while True:
+    DriveButtons()
