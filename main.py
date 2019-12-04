20ad97fcc523eae0c62646e3e6dc9e65e932dcd2
Jordan Miles <J@jmiles42.com>
2019-12-04 16:20:43 +1100

    Started Obsticle avoidance

--- main.py
+++ main.py
@@ -19,11 +19,14 @@ DRIVE_MODE_UPSIDEDOWN = 7
 FORWARD_SPEED = 200
 TURN_SPEED = 100
 FLIP_SPEED = 200
+MOVEMENT_DURATION = 100
 
 class RobotData:
     mode = DRIVE_MODE_STOP
     running = False
     startingRot = 0
+    rotation = 0
+    xVal = 0
     def __init__(self, _mode, _running, _rotation):
         self.mode = _mode
         self.running = _running
@@ -39,6 +42,7 @@ mD = Motor(Port.D)
 sD = UltrasonicSensor(Port.S1)
 sF = UltrasonicSensor(Port.S2)
 sR = GyroSensor(Port.S4)
+sR.reset_angle()
 
 rData = RobotData(DRIVE_MODE_STOP, False, mD.angle())
 
@@ -46,30 +50,25 @@ def Drive(speed):
     mA.run(speed)
     mB.run(speed)
 
+def Drive(speed, time):
+    mA.run_time(speed, MOVEMENT_DURATION)
+    mB.run_time(speed, MOVEMENT_DURATION)
+
 def Turn(turnSpeed):
     mA.run(-turnSpeed)
     mB.run(turnSpeed)
+
+def Turn(turnSpeed):
+    mA.run_time(-turnSpeed, MOVEMENT_DURATION)
+    mB.run_time(turnSpeed, MOVEMENT_DURATION)
     
 def Stop():
     mA.run(0)
     mB.run(0)
 
-def DriveButtons():
-    global rData
-    buttons = brick.buttons()
-    if Button.CENTER in buttons:
-        rData.running = not rData.running
-    if Button.UP in buttons:
-        rData.SetMode(DRIVE_MODE_FORWARD)
-    if Button.DOWN in buttons:
-        rData.SetMode(DRIVE_MODE_BACKWARD)
-    if Button.LEFT in buttons:
-        rData.SetMode(DRIVE_MODE_UPSIDEDOWN)
-    if Button.RIGHT in buttons:
-        rData.SetMode(DRIVE_MODE_UPRIGHT)
-
 def FlipUpright(waitValue):
     mD.run_target(FLIP_SPEED, rData.startingRot, waitValue)
+
 def FlipUpsideDown(waitValue):
     mD.run_target(FLIP_SPEED, rData.startingRot + 190, waitValue)
 
@@ -99,11 +98,41 @@ def DrivingLoop():
 
 t = Thread(target = DrivingLoop)
 t.start()
+isAWall = False
+isACliff = False
+wantedRotation = 0
+hasWantedRotation = False
+
+def RotateTo()
+    global hasWantedRotation
+    currentRot = sR.angle()
+    differenceRot = currentRot - hasWantedRotation
+
+    if differenceRot < 0 or currentRot < -90:
+        TurnRight()
+    elif differenceRot > 0 or currentRot < 90:
+        TurnLeft()
+    else
+        hasRotation = False
+        rData.rotation = sR.angle()
+        hasWantedRotation = 0
 
 while True:
-    if(sD.distance() < 70 and sF.distance() > 100):
-        rData.SetMode(DRIVE_MODE_FORWARD)
+    if hasRotation:
+        RotateTo()
+
+    canDriveFwd = not sF.distance() > 100
+    isACliff = not sD.distance() < 70
+
+    if isACliff:
+        rData.SetMode(DRIVE_MODE_LEFT)
         rData.running = True
+
+        if not hasRotation:
+            wantedRotation = 90 + rData.rotation
+            hasRotation = True
+    elif canDriveFwd:
+        rData.SetMode(DRIVE_MODE_FORWARD)
     else:
         rData.SetMode(DRIVE_MODE_STOP)
         rData.running = False
